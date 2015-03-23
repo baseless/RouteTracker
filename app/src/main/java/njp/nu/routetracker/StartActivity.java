c4d1a3349c6d7c4aa206988b043e5755aa84213c
base <daniel@ryhle.se>
2015-03-23 11:23:21 +0100

    .

---
+++ app/src/main/java/njp/nu/routetracker/StartActivity.java
@@ -0,0 +1,12 @@
+package njp.nu.routetracker;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class StartActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
